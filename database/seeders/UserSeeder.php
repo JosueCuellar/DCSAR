9bf6782da1991eaba1cebcce3b30ab1a5f72edc5
JosueCuellar <cc19114@ues.edu.sv>
2023-08-11 10:45:28 -0600

    Seeders para correr

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,11 @@ class UserSeeder extends Seeder
 	{
 		//
 
-		DB::table('unidad_organizativas')->insert([
-			['nombreUnidadOrganizativa' => 'Unidad Logistica', 'descripUnidadOrganizativa' => 'Unidad Logistica'],
-			['nombreUnidadOrganizativa' => 'Gerencia de Sistemas Informaticos', 'descripUnidadOrganizativa' => 'Gerencia de Sistemas Informaticos'],
-			['nombreUnidadOrganizativa' => 'Gerencia Administracion', 'descripUnidadOrganizativa' => 'Gerencia Administracion'],
-		]);
+		// DB::table('unidad_organizativas')->insert([
+		// 	['nombreUnidadOrganizativa' => 'Unidad Logistica', 'descripUnidadOrganizativa' => 'Unidad Logistica'],
+		// 	['nombreUnidadOrganizativa' => 'Gerencia de Sistemas Informaticos', 'descripUnidadOrganizativa' => 'Gerencia de Sistemas Informaticos'],
+		// 	['nombreUnidadOrganizativa' => 'Gerencia Administracion', 'descripUnidadOrganizativa' => 'Gerencia Administracion'],
+		// ]);
 
 		User::create([
 			'name' => 'Super Admin',
@@ -28,60 +28,60 @@ class UserSeeder extends Seeder
 			'password' => bcrypt('password')
 		])->assignRole('Super Administrador');
 
-		User::create([
-			'name' => 'Gerente Encargado',
-			'unidad_organizativa_id' => 1,
-			'email' => 'gerente@gerente.com',
-			'password' => bcrypt('password')
-		])->assignRole('Gerente Encargado Almacen');
+		// User::create([
+		// 	'name' => 'Gerente Encargado',
+		// 	'unidad_organizativa_id' => 1,
+		// 	'email' => 'gerente@gerente.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Gerente Encargado Almacen');
 
-		User::create([
-			'name' => 'Carlos Rivas',
-			'unidad_organizativa_id' => 1,
-			'email' => 'carlos@carlos.com',
-			'password' => bcrypt('password')
-		])->assignRole('Tecnico Encargado Almacen');
+		// User::create([
+		// 	'name' => 'Carlos Rivas',
+		// 	'unidad_organizativa_id' => 1,
+		// 	'email' => 'carlos@carlos.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Tecnico Encargado Almacen');
 
-		User::create([
-			'name' => 'Josue Ernesto Cruz Cuellar',
-			'unidad_organizativa_id' => 1,
-			'email' => 'josue@josue.com',
-			'password' => bcrypt('password')
-		])->assignRole('Solicitante Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Josue Ernesto Cruz Cuellar',
+		// 	'unidad_organizativa_id' => 1,
+		// 	'email' => 'josue@josue.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Solicitante Unidad Organizativa');
 
-		User::create([
-			'name' => 'Gerente Josue Ernesto Cruz Cuellar',
-			'unidad_organizativa_id' => 1,
-			'email' => 'gerentejosue@gerentejosue.com',
-			'password' => bcrypt('password')
-		])->assignRole('Gerente Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Gerente Josue Ernesto Cruz Cuellar',
+		// 	'unidad_organizativa_id' => 1,
+		// 	'email' => 'gerentejosue@gerentejosue.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Gerente Unidad Organizativa');
 
-		User::create([
-			'name' => 'Ana Cecilia Cuellar Rodriguez',
-			'unidad_organizativa_id' => 2,
-			'email' => 'ana@ana.com',
-			'password' => bcrypt('password')
-		])->assignRole('Solicitante Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Ana Cecilia Cuellar Rodriguez',
+		// 	'unidad_organizativa_id' => 2,
+		// 	'email' => 'ana@ana.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Solicitante Unidad Organizativa');
 
-		User::create([
-			'name' => 'Gerente Cecilia Cuellar Rodriguez',
-			'unidad_organizativa_id' => 2,
-			'email' => 'gerenteana@gerenteana.com',
-			'password' => bcrypt('password')
-		])->assignRole('Gerente Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Gerente Cecilia Cuellar Rodriguez',
+		// 	'unidad_organizativa_id' => 2,
+		// 	'email' => 'gerenteana@gerenteana.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Gerente Unidad Organizativa');
 
-		User::create([
-			'name' => 'Milena Abigail Barahona Mayen',
-			'unidad_organizativa_id' => 3,
-			'email' => 'milena@milena.com',
-			'password' => bcrypt('password')
-		])->assignRole('Solicitante Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Milena Abigail Barahona Mayen',
+		// 	'unidad_organizativa_id' => 3,
+		// 	'email' => 'milena@milena.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Solicitante Unidad Organizativa');
 
-		User::create([
-			'name' => 'Gerente Milena Abigail Barahona Mayen',
-			'unidad_organizativa_id' => 3,
-			'email' => 'gerentemilena@gerentemilena.com',
-			'password' => bcrypt('password')
-		])->assignRole('Gerente Unidad Organizativa');
+		// User::create([
+		// 	'name' => 'Gerente Milena Abigail Barahona Mayen',
+		// 	'unidad_organizativa_id' => 3,
+		// 	'email' => 'gerentemilena@gerentemilena.com',
+		// 	'password' => bcrypt('password')
+		// ])->assignRole('Gerente Unidad Organizativa');
 	}
 }
